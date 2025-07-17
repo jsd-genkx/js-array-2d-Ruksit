1d9ccce8cacd6c896b0004f1f21738d45f5e3c75
Ruksit <ruksitm@gmail.com>
2025-07-17 10:49:29 +0700

    game-fn solution

--- game-fn.js
+++ game-fn.js
@@ -1,25 +1,82 @@
 "use strict";
 
-const board = [
-	["A", "B", "C"],
-	["D", "E", "F"],
-	["G", "H", "I"],
-];
+//sol 1
+// const board = [
+// 	["A", "B", "C"],
+// 	["D", "E", "F"],
+// 	["G", "H", "I"],
+// ];
 
+// let x = 0;
+// let y = 0;
+// const moves = [];
+// moves.push(board[x][y]); // Start at A
+
+// // Define movement functions
+// function moveRight() {
+// y++
+// moves.push(board[x][y]);
+// }
+
+
+// function moveLeft() {
+// y--
+// moves.push(board[x][y]);
+// }
+
+// function moveUp() {
+// x--
+// moves.push(board[x][y]);
+// }
+
+// function moveDown() {
+// x++
+// moves.push(board[x][y]);
+// }
+
+// // Call the movement functions
+// moveRight(); // Move from A to B
+// moveRight(); // Move from B to C
+// moveDown(); // Move from C to F
+// moveLeft(); // Move from F to E
+
+
+// console.log("Path:", moves.join(" → "));
+// console.log("Total Moves (Start from 'A'):", moves.length - 1);
+
+
+
+// Sol 2
+const board = [
+    ["A", "B", "C"],
+    ["D", "E", "F"],
+    ["G", "H", "I"],
+];
 let x = 0;
 let y = 0;
+const xMax = board.length - 1;
+const yMax = board[0].length -1;
 const moves = [];
 moves.push(board[x][y]); // Start at A
 
 // Define movement functions
-function moveRight() {}
+function moveRight() {
+    y < yMax ? moves.push(board[x][++y]) : limitLog('moving right');
+}
+function moveLeft() {
+    y > 0 ? moves.push(board[x][--y]) : limitLog('moving left');
+}
+function moveUp() {
+    x > 0 ? moves.push(board[--x][y]) : limitLog('moving up');
+}
 
-function moveLeft() {}
+function moveDown() {
+    x < xMax ? moves.push(board[++x][y]) : limitLog('moving down');
+}
 
-function moveUp() {}
-
-function moveDown() {}
-
+function limitLog(moving) {
+    console.log( `You have reached the limit of ${moving}.` );
+}
 // Call the movement functions
 moveRight(); // Move from A to B
 moveRight(); // Move from B to C
@@ -28,3 +85,5 @@ moveLeft(); // Move from F to E
 
 console.log("Path:", moves.join(" → "));
 console.log("Total Moves (Start from 'A'):", moves.length - 1);
+
+// -m "game-fn solution"
